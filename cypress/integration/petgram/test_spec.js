bfd25b1d75ee75212c739aaf1358d9466d1cbbe4
clagarejo <clagarejo89@gmail.com>
2021-05-24 17:12:49 -0500

    deploy

--- cypress/integration/petgram/test_spec.js
+++ cypress/integration/petgram/test_spec.js
@@ -1,22 +1,19 @@
 /* global describe, it, cy */
 
 describe('Petgram', function () {
-  it('Para ver si la app funciona', function (params) {
+  it('Para ver si la app funciona', function () {
     cy.visit('/')
   })
-
-  it('navegamos a una categoria y vemos fotos', function (params) {
+  it('Navegamos a una categoría y vemos fotos', function () {
     cy.visit('/pet/1')
-    cy.gey('article')
+    cy.get('article')
   })
-
-  it('si podemos navegar con la navbar a la home', function (params) {
+  it('Si podemos navegar con la navbar a la home', function () {
     cy.visit('/pet/1')
     cy.get('nav a').first().click()
     cy.url().should('include', '/')
   })
-
-  it('los usuarios no registrados ven el formulario de registro e inicio de sesion', function (params) {
+  it('Los usuarios no registrados ven el formulario de registro al ir a favs', function () {
     cy.visit('/favs')
     cy.get('form').should('have.length', 2)
   })
